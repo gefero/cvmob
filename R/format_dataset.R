ecff2cf8b9e02afe1c347c78861910d390e1ee31
gefero <german.rosati@gmail.com>
2019-03-06 16:12:00 -0300

    Updt format_dataset

--- R/format_dataset.R
+++ R/format_dataset.R
@@ -5,23 +5,27 @@
 #' @return un dataframe formateado
 #' @export
 
-
 format_dataset <- function(path){
-        df <- foreign::read.spss(path, to.data.frame = TRUE)
-        df$aglomerado1<-as.integer(df$aglomerado)
-        df$tamano1<-as.integer(df[['tama\u00f1o']])
+        df <- foreign::read.spss(path, to.data.frame = TRUE, use.value.labels = FALSE)
+
+        df <- dplyr::select(df, -(cons_01:filter_.))
+
+        #df$aglomerado1<-as.integer(df$aglomerado)
+        df$tamano<-df[['tama\u00f1o']]
         id <- c(1,2,3,4,5,6,7,10)
         df$forzoso <- as.integer(df$aglomerado) %in% id
 
         df$estrato_e1<-NA
-        df$estrato_e1[df$aglomerado1==1] <- 'AMBA'
-        df$estrato_e1[df$aglomerado1==2] <- 'Gran CBA'
-        df$estrato_e1[df$aglomerado1==3] <- 'Gran Rosario'
-        df$estrato_e1[df$aglomerado1==4] <- 'Gran Mendoza'
-        df$estrato_e1[df$aglomerado1==5] <- 'Gran La Plata'
-        df$estrato_e1[df$aglomerado1==6] <- 'Gran SM Tucuman'
-        df$estrato_e1[df$aglomerado1==7] <- 'Gran Mar del Plata'
-        df$estrato_e1[df$aglomerado1==10] <- 'Gran Santa Fe'
+        df$estrato_e1[df$aglomerado=='Área Metropolitana de Buenos Aires'] <- 'AMBA'
+        df$estrato_e1[df$aglomerado=='Gran Córdoba'] <- 'Gran CBA'
+        df$estrato_e1[df$aglomerado=='Gran Rosario'] <- 'Gran Rosario'
+        df$estrato_e1[df$aglomerado=='Gran Mendoza'] <- 'Gran Mendoza'
+        df$estrato_e1[df$aglomerado=='Gran La Plata'] <- 'Gran La Plata'
+        df$estrato_e1[df$aglomerado=='Gran San Miguel de Tucumán'] <- 'Gran SM Tucuman'
+        df$estrato_e1[df$aglomerado=='Gran Mar del Plata'] <- 'Gran Mar del Plata'
+        df$estrato_e1[df$aglomerado=='Gran Santa Fe'] <- 'Gran Santa Fe'
+
+        ## REVISAR
         df$estrato_e1[df$urbrur=='Urbano' & df$tamano1==2 & df$forzoso==0] <- "Aglo > 500000"
         df$estrato_e1[df$urbrur=='Urbano' & df$tamano1==3 & df$forzoso==0] <- "Aglo > 50000 y < 500000"
         df$estrato_e1[df$urbrur=='Urbano' & df$tamano1==4 & df$forzoso==0] <- "Aglo > 50000 y < 500000"
@@ -34,3 +38,4 @@ format_dataset <- function(path){
 
         return(df)
 }
+
