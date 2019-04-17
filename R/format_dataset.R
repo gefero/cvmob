afee6c2e4b2fa26548bccae13c92979b1680d232
gefero <german.rosati@gmail.com>
2019-04-17 16:52:01 -0300

    Updt estratos

--- R/format_dataset.R
+++ R/format_dataset.R
@@ -9,12 +9,14 @@ format_dataset <- function(path){
         df <- foreign::read.spss(path, to.data.frame = TRUE, use.value.labels = TRUE)
 
 
-        idf <- c('AMBA', 'Gran CBA', 'Gran Rosario', 'Gran Mendoza', 'Gran La Plata',
+        idf <- c('AMBA + GCba', 'Gran Rosario', 'Gran Mendoza', 'Gran La Plata',
                  'Gran SM Tucuman', 'Gran Mar del Plata', 'Gran Santa Fe')
 
+        df$base<-droplevels(df$base)
+
         df$estrato_e1<-NA
-        df$estrato_e1[df$aglomerado=='\u00c1rea Metropolitana de Buenos Aires'] <- 'AMBA'
-        df$estrato_e1[df$aglomerado=='Gran C\u00f3rdoba'] <- 'Gran CBA'
+        df$estrato_e1[df$aglomerado=='\u00c1rea Metropolitana de Buenos Aires'] <- 'AMBA + GCba'
+        df$estrato_e1[df$aglomerado=='Gran C\u00f3rdoba'] <- 'AMBA + GCba'
         df$estrato_e1[df$aglomerado=='Gran Rosario'] <- 'Gran Rosario'
         df$estrato_e1[df$aglomerado=='Gran Mendoza'] <- 'Gran Mendoza'
         df$estrato_e1[df$aglomerado=='Gran La Plata'] <- 'Gran La Plata'
@@ -25,11 +27,11 @@ format_dataset <- function(path){
         df$forzoso <- df$estrato_e1 %in% idf
 
         ## REVISAR
-        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con m\u00e1s de medio mill\u00f3n de habitantes (no AMBA)' & df$forzoso==0] <- "Aglo > 500.000"
-        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de medio mill\u00f3n de habitantes y m\u00e1s de cien mil' & df$forzoso==0] <- "Aglo > 100.000 y < 500.000"
-        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de cien mil habitantes y m\u00e1s de cincuenta mil' & df$forzoso==0] <- "Aglo > 50.000 y < 100.000"
-        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de cincuenta mil habitantes y m\u00e1s de dos mil' & df$forzoso==0] <- "Algo > 2.000 y < 50.000"
-        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de dos mil habitantes y poblaci\u00f3n rural' & df$forzoso==0] <- "Aglo < 2.000 y rural"
+        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con m\u00e1s de medio mill\u00f3n de habitantes (no AMBA)' & df$forzoso==FALSE] <- "Aglo > 500.000"
+        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de medio mill\u00f3n de habitantes y m\u00e1s de cien mil' & df$forzoso==FALSE] <- "Aglo > 100.000 y < 500.000"
+        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de cien mil habitantes y m\u00e1s de cincuenta mil' & df$forzoso==FALSE] <- "Aglo > 50.000 y < 100.000"
+        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de cincuenta mil habitantes y m\u00e1s de dos mil' & df$forzoso==FALSE] <- "Algo > 2.000 y < 50.000"
+        df$estrato_e1[df$urbrur=='Urbano' & df[['tama\u00f1o']]=='Aglomerados con menos de dos mil habitantes y poblaci\u00f3n rural' & df$forzoso==FALSE] <- "Aglo < 2.000 y rural"
         df$estrato_e1[df$urbrur=='Rural'] <- 'Aglo < 2.000 y rural'
 
         df$estrato_final <- paste(df$base, df$estrato_e1)
